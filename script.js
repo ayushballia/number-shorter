587ebf511423fa9e9c25fa96ff40197d8478180e
Ayush Tiwari <ayushtiwari@live.in>
2024-05-16 22:49:43 +0530

    selection sort

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  const sortedValues = bubbleSort(inputValues);
+  const sortedValues = selectionSort(inputValues);
   updateUI(sortedValues);
 };
 
@@ -29,5 +29,24 @@ const bubbleSort = (array) => {
   }
 
   return array;
+};
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array
 };
 sortButton.addEventListener("click", sortInputArray);
